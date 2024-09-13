140e26b89229d690784a7b87153f9c3dfde437b5
EfremovAS <Alexef3@mail.ru>
2024-09-14 02:38:53 +0300

    Added get_access_token

--- src/api/auth.py
+++ src/api/auth.py
@@ -43,4 +43,5 @@ async def login_user(
     
 @router.get("/only_auth")
 async def only_auth(request: Request):
-    ...
+    access_token = request.cookies.get("access_token") or None
+    return {"access_token": access_token}
